d0110785cd3d2db5c0b65490724ab8b7062230d4
Mahomed Ismail <lordismail.iii@gmail.com>
2023-02-17 15:09:32 -0800

    Using puts()

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char var [] = {"Programming is like building a multilingual puzzle\n"}
+puts(var);
+return (0);
+}
