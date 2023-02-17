0a4c671f4ee7455d7e06b9161ecae874e49729cb
Mahomed Ismail <lordismail.iii@gmail.com>
2023-02-17 15:17:33 -0800

    using puts() func.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-char var [] = {"Programming is like building a multilingual puzzle\n"};
+char var [] = {"\"Programming is like building a multilingual puzzle\n"};
 puts(var);
 return (0);
 }
