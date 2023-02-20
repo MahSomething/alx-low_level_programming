3e53fcabedbb516953d6d7dcf0946f4d695d1743
Mahomed Ismail <lordismail.iii@gmail.com>
2023-02-20 11:55:05 -0800

    Alphabet Low and Upper cases

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void)
+{
+	char a = 'a';
+	char b = 'A';
+	char c = '\n';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	while ( b <= 'Z')
+		{
+		putchar(b);
+		b++;
+		}
+	putchar(c);
+return (0);
+}
