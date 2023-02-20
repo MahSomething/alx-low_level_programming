c365c64e7931ce5bf39b77c366507d730ee54b32
Mahomed Ismail <lordismail.iii@gmail.com>
2023-02-20 07:52:34 -0800

    Finding the last degit of a Random Number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,10 +13,10 @@
 int main(void)
 {
 int n;
+int number = n % 10;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
-int number = n % 10;
 if (number > 5)
 	printf("Last digit of %d is %d  and is greater than 5\n", n, number);
 else if (number == 0)
