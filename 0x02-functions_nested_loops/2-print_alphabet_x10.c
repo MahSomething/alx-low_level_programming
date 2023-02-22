974b6a370bee12621a30f071dfdbab524a64cc2b
Mahomed Ismail <lordismail.iii@gmail.com>
2023-02-22 08:05:45 -0800

    Function to print x10 english Alphabet fix3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
-*print_alphabet_10x - will run the loop and send the char to _putchar
+*print_alphabet_x10 - will run the loop and send the char to _putchar
 */
 void print_alphabet_x10(void)
 {
 char i;
 int j = 1;
+
 while (j <= 10)
-	{
-		for (i = 'a'; i <= 'z'; i++)
-		{
-		_putchar(i);
-		}
-	_putchar('\n');
-	j++;
-	}
+{
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+j++;
+}
 }
