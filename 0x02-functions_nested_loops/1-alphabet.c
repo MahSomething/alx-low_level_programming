b22334b4fb9809c1d69a8d0cae85eb48f51164c2
Mahomed Ismail <lordismail.iii@gmail.com>
2023-02-22 07:14:44 -0800

    Function to print all english alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,23 +1,12 @@
 #include "main.h"
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
 
-/*
-*void print_alphabet() - will print the english aphabet
-*/
-void print_alphabet(void)
+void print_alphabet() /*void print_alphabet() - will print the english aphabet */
 {
-/**
-*letter - will interrate throug all the ASCII code
-*/
 char i;
-for (i ='a'; i <= 'z'; i++)
+for (i = 'a'; i <= 'z'; i++)
 {
-/**
-* print_alphabet - will run the loop and send the char to _putchar 
+/*
+* print_alphabet - will run the loop and send the char to _putchar
 */
 _putchar(i);
 }
