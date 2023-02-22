8591636249b09c8ab1615ad420d83163ea4c3718
Mahomed Ismail <lordismail.iii@gmail.com>
2023-02-22 05:11:34 -0800

    Function that print the english alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -11,14 +11,6 @@ int main(void)
  *
  * Return: Always 0.
  */
-	void print_alphabet(void)
-	{
-	char letter;
-	for (letter = 'a'; letter <= 'z'; letter++)
-		{
-		_putchar(letter);
-		}
-		_putchar('\n');
-	}
+print_alphabet()
 return (0);
 }
