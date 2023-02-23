156e44ecdacfcd3af57decb5aac8641804f4f6ac
Mahomed Ismail <lordismail.iii@gmail.com>
2023-02-22 16:30:21 -0800

    A function that returns the last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *print_last_digit - Returns the last digit of any number
+ * @number: is a in intiger value
+ *
+ * Return: 1 for lowercase. 0 for the rest.
+ */
+int print_last_digit(int number)
+{
+number = number % 10;
+return (0);
+}
