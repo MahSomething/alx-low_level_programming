896f00f4ad43592b7f78e98548f254d52420b108
Mahomed Ismail <lordismail.iii@gmail.com>
2023-02-23 08:06:22 -0800

    Diferent main to test positive_or_negative() for 0 case

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,3 +1,12 @@
 #include "main.h"
-if (int i == 0)
-{void positive_or_negative(i)};
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
