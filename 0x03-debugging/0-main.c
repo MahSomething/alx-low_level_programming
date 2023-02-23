151c3f6d0b31fa57f9dd7a0b39ac07e35faf2384
Mahomed Ismail <lordismail.iii@gmail.com>
2023-02-23 09:08:13 -0800

    Main file to check case 0 for positive_or_ngegative funct.

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
 * main - tests function that prints if integer is 0
+* @c:  - will pass the value to function to be checked
 * Return: 0
 */
-
-void positive_or_negative(int);
+void positive_or_negative(int c);
 
 int main(void)
 {
