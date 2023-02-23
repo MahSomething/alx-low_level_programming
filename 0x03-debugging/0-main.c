33e091d65cc4e976fa6f0a22aef60892279ac6b3
Mahomed Ismail <lordismail.iii@gmail.com>
2023-02-23 14:24:46 -0800

    Main file that check for case 0 in functionPosetive_or_Negative

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,15 +3,14 @@
  * main - tests function prints if integer positive or negative
  * Return: 0
  */
+
 int main(void)
 {
-char c = '\n';
-int i;
 
-if (i == 0)
-{
+int i;
+i = 0;
+
 positive_or_negative(i);
-putchar(c);
-}
+
 return (0);
 }
