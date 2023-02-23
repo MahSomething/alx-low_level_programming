46f22027ffc2258883bc4ac84f676d209c10f4cb
Mahomed Ismail <lordismail.iii@gmail.com>
2023-02-22 16:09:16 -0800

    Function that finds the Absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_abs - Returns the Absolute value of any number
+ * @number: is a in intiger value
+ *
+ * Return: 1 for lowercase. 0 for the rest.
+ */
+int _abs(int number)
+{
+if (number < 0)
+{
+number = number * (-1);
+}
+else
+number = number;
+return (0);
+}
