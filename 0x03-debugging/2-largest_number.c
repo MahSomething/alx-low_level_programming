194852010e02c093c191d92dcd7b8d7cc808f014
Mahomed Ismail <lordismail.iii@gmail.com>
2023-02-24 08:57:47 -0800

    Header file update

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,4 +1,4 @@
-int largest_number(int , int , int);
+int largest_number(int, int, int);
 #include "main.h"
 
 /**
@@ -11,20 +11,20 @@ int largest_number(int , int , int);
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
 
-    if (a > b && b > c)
-    {
-        largest = a;
-    }
-    else if (b > a && a > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
+	if (a > b && b > c)
+	{
+	largest = a;
+	}
+	else if (b > a && a > c)
+	{
+	largest = b;
+	}
+	else
+	{
+	largest = c;
+	}
 
-    return (largest);
+return (largest);
 }
