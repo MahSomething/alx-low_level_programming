d55ca4884d61ee8843c27803220682ca8d326762
Mahomed Ismail <lordismail.iii@gmail.com>
2023-02-24 04:50:59 -0800

    Finding Infinite Loop fix1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,21 +2,23 @@
 * main - causes an infinite loop
 * Return: 0
 */
-
 int main(void)
 {
 int i;
 
 printf("Infinite loop incoming :(\n");
 /**
+*Infinite loop - The cause of Infinite Loop
 *i = 0;
 *
 *while (i < 10)
 *{
 *
 *putchar(i);
-*
 *}
+*/
+/**
+* @\\o/\n - Print a new line
 */
 printf("Infinite loop avoided! \\o/\n");
 
