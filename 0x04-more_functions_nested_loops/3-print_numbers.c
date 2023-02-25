2cb5de19548863b540ec01d0eb3f92acc5ac5cb7
Mahomed Ismail <lordismail.iii@gmail.com>
2023-02-25 14:21:30 -0800

    A function that prints numbers from 0 to 9 in characters

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,5 @@
 /**
 * print_numbers - this function is used to print numbers as characters
-* @number: used to save the iterrated numbers
-* @newLine: is used to print the new line
 */
 void print_numbers(void)
 {
