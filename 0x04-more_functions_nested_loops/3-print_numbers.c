d1b5e6d36ab3f84a97635c7b97119e5fe2f9e726
Mahomed Ismail <lordismail.iii@gmail.com>
2023-02-25 14:15:51 -0800

    A function that prints numbers from 0 to 9 in characters

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,13 @@
-#include "main.h"
+/**
+* print_numbers - this function is used to print numbers as characters
+* @number: used to save the iterrated numbers
+* @newLine: is used to print the new line
+*/
 void print_numbers(void)
 {
 char number;
 char newLine = '\n';
+
 for (number = '0'; number <= '9'; number++)
 {
 	_putchar(number);
