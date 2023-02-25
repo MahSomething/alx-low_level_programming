fad0489d0a585e275127c01478ce428698edb2f6
Mahomed Ismail <lordismail.iii@gmail.com>
2023-02-25 15:54:59 -0800

    Function to print numbers from 0 to 9 except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_numbers - this function is used to print numbers as characters
+*/
+void print_numbers(void)
+{
+char number;
+char newLine = '\n';
+
+for (number = '0'; number <= '9'; number++)
+{
+	if (number != '2' && number != '4')
+	{
+	_putchar(number);
+	}
+}
+	_putchar(newLine);
+}
