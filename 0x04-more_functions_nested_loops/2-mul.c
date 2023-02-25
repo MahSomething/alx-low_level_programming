7020440e292db02f456ba026ad61c0ac7bfaf577
Mahomed Ismail <lordismail.iii@gmail.com>
2023-02-25 13:53:37 -0800

    Function to multiply 2 numbers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,3 +1,9 @@
+/**
+* mul - A function to multiply 2 digits
+* @a: is the variable that receives the 1st value
+* @b: is the variable that receives the 2nd value
+* Return: the Multiplication of a and b
+*/
 int mul(int a, int b)
 {
 	return (a * b);
