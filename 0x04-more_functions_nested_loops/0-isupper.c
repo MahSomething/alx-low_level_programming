7ed21b8358255f425f22adf55dccfcf3a355e2a2
Mahomed Ismail <lordismail.iii@gmail.com>
2023-02-25 13:13:04 -0800

    function to check if the letter is Upper or lowercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _isupper - is a function used to check and return for uppercase letters
+* @c: is used to receive the value to be checked by the _isupper()
+* Return: zero
+*/
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+return (0);
+}
