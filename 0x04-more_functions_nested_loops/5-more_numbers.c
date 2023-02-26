9a7de267ce806ceb9e71654b1bc7d34fb5c4fa74
Mahomed Ismail <lordismail.iii@gmail.com>
2023-02-26 11:30:38 -0800

    Printing from 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+void more_numbers(void)
+{
+	int i;
+	int j;
+	char bewLine = '\n';
+for (i = 0; i <= 10; i++)
+	{
+	for (j = 0; j <= 15; j++)
+	{
+		if ( j < 9)
+			{
+			_putchar(j%10 + '0');
+			}
+		_putchar(j/10 + '0');
+	}
+	_putchar(newLine);
+	}
+}
