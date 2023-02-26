e77388a7359f926976cde4b557d935c62e4278cc
Mahomed Ismail <lordismail.iii@gmail.com>
2023-02-26 14:10:10 -0800

    function that prints underscores based on the value of n

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /**
-* print_line - Is a function used to print the n of lines based on the value of n
+* print_line - a function used to print the n of lines based on the value of n
+* @n: Will receive the number of times the function should print _
 */
 void print_line(int n)
 {
 	int i = 1;
 	char newLine = '\n';
 	char underScore = '_';
-	while ( i <= n)
+
+	while (i <= n)
 		{
 		_putchar(underScore);
 		i++;
