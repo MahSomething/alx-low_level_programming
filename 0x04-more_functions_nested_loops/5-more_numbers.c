d2d1b74346d7a70c88fff1085e10876c97e73b89
Mahomed Ismail <lordismail.iii@gmail.com>
2023-02-26 13:00:20 -0800

    Fix function to print from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,9 +8,9 @@ void more_numbers(void)
 	int i;
 	int j;
 	char bewLine = '\n';
-for (i = 0; i <= 10; i++)
+for (i = 0; i <= 9; i++)
 	{
-	for (j = 0; j <= 15; j++)
+	for (j = 0; j <= 14; j++)
 	{
 		if (j < 9)
 			{
