8656d9680231af2e068e331213971209d5e8abc4
Mahomed Ismail <lordismail.iii@gmail.com>
2023-02-28 15:41:03 -0800

    Implementation of puts() fix1

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,8 @@
 #include "main.h"
+#include <unistd.h>
 
 void _puts(char *str)
 {
-int sizeOfStrng = _strlen(strs);
-return (write(1, &str, sizeOfStrng);
+int sizeOfStrng = _strlen(str);
+ write(1, &str, sizeOfStrng);
 }
