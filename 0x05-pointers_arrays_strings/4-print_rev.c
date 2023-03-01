52b648e2a97240b01959f28bd511db8a4dc6383e
Mahomed Ismail <lordismail.iii@gmail.com>
2023-03-01 15:23:57 -0800

    Implementation of Rev()

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,15 +2,17 @@
 * print_rev - usee to reverse a string
 * @s: a pointer that holds the address of a strng
 */
+#include <unistd.h>
 
 void print_rev(char *s)
 {
 int count = 0;
 int i = 0;
+char newLine = '\n';
 while (s[i] != '\0')
 {
 i++;
-count++
+count++;
 }
 
 while (s[i - 1] <= count)
@@ -18,4 +20,5 @@ while (s[i - 1] <= count)
 i--;
 }
 write(1, s, count);
+write(1, &newLine, 1);
 }
